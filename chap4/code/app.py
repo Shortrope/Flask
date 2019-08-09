48161cc3bc75d8971ad6c201b51109ce9144f1f0
Shortrope <mario@shortrope.com>
2019-08-09 04:09:33 +0000

    Start new flask Restful

--- chap4/code/app.py
+++ chap4/code/app.py
@@ -4,10 +4,27 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
+items = []
 
-api.add_resource(Student, '/student/<string:name>')
+class Item(Resource):
+    def get(self, name):
+        if items:
+            for item in items:
+                if name == item['name']:
+                    return item
+        return {'item': None}, 404
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.99}
+        items.append(item)
+        return item, 201
+
+    def put(self, name):
+        pass
+
+    def delete(self, name):
+        pass
+
+api.add_resource(Item, '/item/<string:name>')
 
 app.run(host='192.168.1.10', port=5000)
