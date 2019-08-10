5aac9e715958869561a8402450aa73a4ce0235d5
Shortrope <mario@shortrope.com>
2019-08-10 23:45:59 +0000

    chap4 app.py: improvements
    Use 'filter' and 'next' function
    Return codes: 200, 201, 400, 404
    Add class ItemList

--- chap4/code/app.py
+++ chap4/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -8,15 +8,18 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        if items:
-            for item in items:
-                if name == item['name']:
-                    return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.99}
+
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "An item w name '{}' exists.".format(name)}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
+
         return item, 201
 
     def put(self, name):
@@ -25,6 +28,15 @@ class Item(Resource):
     def delete(self, name):
         pass
 
-api.add_resource(Item, '/item/<string:name>')
 
-app.run(host='192.168.1.10', port=5000)
+class ItemList(Resource):
+    def get(self):
+        if items:
+            return {'items': items}
+        return {'items': None}, 404
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(host='192.168.1.10', port=5000, debug=True)
+
