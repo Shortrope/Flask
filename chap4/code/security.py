323eb8cd6d4f96a9d6d186dcc1b566c175fafad3
Shortrope <mario@shortrope.com>
2019-08-11 19:14:13 +0000

    implement JWT token

--- chap4/code/security.py
+++ chap4/code/security.py
@@ -1,5 +1,5 @@
 from werkzeug.security import safe_str_cmp
-from users import Users
+from user import User
 
 
 users = [
@@ -10,12 +10,12 @@ username_mapping = {user.username: user for user in users}
 
 userid_mapping = {user.id: user for user in users} 
 
-def authentication(username, password):
+def authenticate(username, password):
     user = username_mapping.get(username, None)
     #if user and user.password == password:
     if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):      # 'payload' is the contents of the JWT token
-    user_id = payload['idendity']
+    user_id = payload['identity']
     return userid_mapping.get(user_id, None)
